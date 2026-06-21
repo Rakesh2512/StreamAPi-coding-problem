9d13b3b9585efca9814abdeae824432ccbe0f90e
Rakesh Paswan <vikash25kumarpa@gmail.com>
2026-06-21 18:07:35 +0530

    prime

---
+++ CheckPrimeNumber.java
@@ -0,0 +1,25 @@
+import java.util.stream.IntStream;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int n){
+
+        if(n <= 1){
+            return false;
+        }
+        return IntStream.rangeClosed(2, (int)Math.sqrt(n)).noneMatch(i -> n % i == 0);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i=1;i<=10;i++){
+            if(check(i)){
+                System.out.println("Given number "+i+" is prime number");
+            }
+            else{
+                System.out.println("Given number"+i+" is not prime number");
+            }
+        }
+        
+    }
+}
